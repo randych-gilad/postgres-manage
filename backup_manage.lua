42c24dc0dd36b4ed9a4d333f366b5aeda31ca4da
randych-gilad <xadaka@gmail.com>
2024-01-05 23:40:06 +0200

    create folder function

--- backup_manage.lua
+++ backup_manage.lua
@@ -17,3 +17,23 @@ local isAlreadyExisting = function()
   exist_next = trim_table(exist_next)
   return exist_first, exist_next
 end
+function CreatePgDumpFolder()
+  local first, next = isAlreadyExisting()
+  local total_backups = #next + 1
+  local MAX_BACKUPS = 10
+  if not first then
+    os.execute(string.format("mkdir %s", date))
+  end
+  if not first and next then
+    os.execute(string.format("mkdir %s.1", date))
+  end
+  if total_backups == MAX_BACKUPS then
+    print("Removed 1 oldest backup.")
+    os.execute(string.format("rm -rf %s.*", date))
+    os.execute(string.format("mv %s %s.%s", date, date, total_backups))
+    os.execute(string.format("mkdir %s", date))
+  elseif next then
+    print(string.format("There is already %s backups for %s.", total_backups, date))
+    print(string.format("Created backup %s.", total_backups + 1))
+  end
+end
