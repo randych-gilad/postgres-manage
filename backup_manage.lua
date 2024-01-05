2d9d760e95ff8d6483a542707727d45ce4783a8b
randych-gilad <xadaka@gmail.com>
2024-01-05 23:39:35 +0200

    backup folder existence check 🧐

--- backup_manage.lua
+++ backup_manage.lua
@@ -1,1 +1,19 @@
 local date = os.date("%Y%m%d")
+local isAlreadyExisting = function()
+  local function trim_find(find)
+    return find:gsub("./", "")
+  end
+  local function trim_table(find)
+    local mytable = {}
+    for line in find:gmatch("[^\r\n]+") do
+      line = trim_find(line)
+      table.insert(mytable, line)
+    end
+    return mytable
+  end
+  local exist_first = io.popen(string.format("find . -mindepth 1 -type d -name '%s'", date)):read("*a")
+  exist_first = trim_find(exist_first):gsub("\n", "")
+  local exist_next = io.popen(string.format("find . -mindepth 1 -type d -name '%s.*'", date)):read("*a")
+  exist_next = trim_table(exist_next)
+  return exist_first, exist_next
+end
