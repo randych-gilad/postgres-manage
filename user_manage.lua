231c9f8d9f579f56f1a723ce357e6cbfa620701f
randych-gilad <xadaka@gmail.com>
2024-01-05 23:30:29 +0200

    verify result

--- user_manage.lua
+++ user_manage.lua
@@ -24,17 +24,42 @@ function ValidatePassword()
   end
 end
 
-while true do
-  local input = io.read()
-  if input == "y" then
-    print(string.format("Created .sql script for %s.\n", user))
-    break
-  elseif input == "n" then
-    os.remove(file_name)
-    print(string.format("Discarded .sql script for %s.\n", user))
-    return
-  else
-    print("Only 'y' or 'n' is accepted.")
-    input = io.read()
+function VerifyResult(user, file_name)
+  local file = assert(io.open(file_name, "r"))
+  local content = file:read("*all")
+  print("\n" .. content)
+  file:close()
+  io.write("Is this OK? (y/n) ")
+  while true do
+    local input = io.read()
+    if input == "y" then
+      print(string.format("Created .sql script for %s.\n", user))
+      break
+    elseif input == "n" then
+      os.remove(file_name)
+      print(string.format("Discarded .sql script for %s.\n", user))
+      return
+    else
+      print("Only 'y' or 'n' is accepted.")
+      input = io.read()
+    end
+  end
+
+  io.write("Apply? (y/n) ")
+  local command = string.format("psql -U postgres -f %s", file_name)
+  while true do
+    local input = io.read()
+    if input == "y" then
+      print()
+      os.execute(command)
+      print()
+      break
+    elseif input == "n" then
+      os.remove(file_name)
+      print(string.format("Discarded .sql script for %s.\n", user))
+      break
+    else
+      print("Only 'y' or 'n' is accepted.")
+    end
   end
 end
