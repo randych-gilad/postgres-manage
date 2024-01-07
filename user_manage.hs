26a4e8687ead60d137c10da965a3924de9c954ce
randych-gilad <xadaka@gmail.com>
2024-01-07 09:50:40 +0200

    more reasonable validation

--- user_manage.hs
+++ user_manage.hs
@@ -1,5 +1,5 @@
-import Control.Monad.RWS (MonadWriter (pass))
 import Data.List (isInfixOf)
+import System.Exit (exitFailure, exitSuccess)
 import System.IO (hFlush, stdout)
 import Text.Printf (printf)
 
@@ -7,8 +7,10 @@ main :: IO ()
 main = do
   promptString "Enter username: "
   user <- getLine
+  validateUser user
   promptString "Enter password: "
   password <- getLine
+  validatePassword password
   createUser user password dbs
 
 promptString :: String -> IO ()
@@ -19,19 +21,23 @@ promptString msg = do
 dbs :: [String]
 dbs = ["kek", "kekus"]
 
-validateUser :: String -> Bool
-validateUser "postgres" = False
-validateUser user = not ("postgres" `isInfixOf` user)
+validateUser :: String -> IO String
+validateUser user
+  | "postgres" `isInfixOf` user = do
+      putStrLn "Username cannot contain postgres"
+      exitFailure
+  | otherwise = return user
 
-validatePassword :: String -> Bool
-validatePassword password = length password >= 9
+validatePassword :: String -> IO String
+validatePassword password
+  | length password < 9 = do
+      putStrLn "Password too short"
+      exitFailure
+  | otherwise = return password
 
 createUser :: String -> String -> [String] -> IO ()
-createUser user password dbs
-  | not $ validateUser user = putStrLn "Username cannot contain postgres"
-  | not $ validatePassword password = putStrLn "Password too short"
-  | otherwise = do
-      writeFile file_name $ printf "CREATE USER %s WITH PASSWORD '%s';\n" user password
+createUser user password dbs = do
+  writeFile file_name $ printf "CREATE USER %s WITH PASSWORD '%s';\n" user password
   where
     -- mapM_ putStrLn $ map (\db -> db ++ ".sql") dbs
 
