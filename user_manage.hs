cf73ef222ed93418260e10b68952913337412298
randych-gilad <xadaka@gmail.com>
2024-01-07 08:24:13 +0200

    haskell is hard

--- user_manage.hs
+++ user_manage.hs
@@ -1,3 +1,28 @@
+import Data.List (isInfixOf)
+import System.IO (hFlush, stdout)
+import Text.Printf (printf)
+
 main :: IO ()
 main = do
-  print 0
+  putStr "Enter username: "
+  hFlush stdout
+  user <- getLine
+  print $ validateUser user
+  print $ createUser
+
+dbs :: [String]
+dbs = []
+
+validateUser :: String -> Bool
+validateUser "postgres" = False
+validateUser user = not ("postgres" `isInfixOf` user)
+
+validatePassword :: String -> Bool
+validatePassword password = length password < 9
+
+createUser :: String -> String -> [String] -> IO ()
+createUser user password db
+  | not validateUser user = ()
+  | not validatePassword password = ()
+  | otherwise = do
+      mapM_ putStrLn $ map (\db -> db ++ ".sql") dbs
