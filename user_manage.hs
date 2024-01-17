f8380baf11606b205f41d0bb6cad3b1f0849a116
Randych <59070097+randych-gilad@users.noreply.github.com>
2024-01-17 22:22:25 +0200

    linear file creation -> prompt (it's bad and not expandable)

--- user_manage.hs
+++ user_manage.hs
@@ -4,7 +4,7 @@ import Data.List (isInfixOf)
 import Data.Maybe (isNothing)
 import System.Directory (removeFile)
 import System.Environment (lookupEnv)
-import System.Exit (exitFailure)
+import System.Exit (exitFailure, exitSuccess)
 import System.IO (hFlush, stdout)
 import Text.Printf (printf)
 
@@ -27,14 +27,7 @@ main = do
       putStrLn "Password too short"
       exitFailure
     pure password_input
-  writeFile (user ++ ".sql") $
-    createUserSQL user password inputsDB
-  writeFile ("revoke_" ++ user ++ ".sql") $
-    revokeUserSQL user inputsDB
-  writeFile ("chpw_" ++ user ++ ".sql") $
-    changePassword user password
-  result <- verifyResult user $ user ++ ".sql"
-  putStrLn result
+  promptAction user password inputsDB
 
 displayMessage :: String -> IO ()
 displayMessage message = do
@@ -47,7 +40,8 @@ inputsDB = []
 inputsEnvVar :: [String]
 inputsEnvVar = ["PGHOST", "PGPASSWORD"]
 
-promptAction = do
+promptAction :: String -> String -> [String] -> IO ()
+promptAction user password dbs = do
   putStrLn "Choose action:"
   putStrLn "[1] Create user"
   putStrLn "[2] Revoke user"
@@ -55,9 +49,36 @@ promptAction = do
   putStrLn "[q] Exit\n"
   putStr "Selected option: "
   action <- getLine
-  pure ()
+  case action of
+    "1" -> do
+      writeFile file_name $
+        createUserSQL user password dbs
+      result <- verifyResult user file_name
+      print result
+      where
+        file_name = user ++ ".sql"
+    "2" -> do
+      writeFile file_name $
+        revokeUserSQL user dbs
+      result <- verifyResult user file_name
+      print result
+      where
+        file_name = "revoke_" ++ user ++ ".sql"
+    "3" -> do
+      writeFile file_name $
+        changePassword user password
+      result <- verifyResult user file_name
+      print result
+      where
+        file_name = "chpw_" ++ user ++ ".sql"
+    "q" -> do
+      putStrLn "Exiting...\n"
+      exitSuccess
+    _ -> do
+      putStrLn "Unsupported input\n"
+      promptAction user password dbs
 
-verifyResult :: String -> FilePath -> IO String
+verifyResult :: String -> FilePath -> IO Bool
 verifyResult user file_name = do
   putStrLn ""
   content <- readFile file_name
@@ -65,10 +86,15 @@ verifyResult user file_name = do
   putStrLn "Is this OK? (y/n)"
   answer <- getLine
   case answer of
-    "y" -> pure content
+    "y" -> do
+      undefined
+      pure True -- should proceed to applyResult here but no idea how
+      -- I don't feel like supplying more arguments just for sake of this
+      -- while it should be own FilePath -> IO () function
     "n" -> do
       removeFile file_name
-      pure $ printf "Discarded .sql script for %s." user
+      putStrLn $ "Discarded .sql script for " ++ user
+      pure False
     _ -> do
       putStrLn "Only y/n are accepted."
       verifyResult user file_name
